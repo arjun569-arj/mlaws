1c3e7126557ddcc7c9073053a948d1004a69114b
arjun <arjun569@gmail.com>
2025-02-01 21:49:16 +0530

    Initial commit

---
+++ learning .py
@@ -0,0 +1,5 @@
+import streamlit as st
+st.title('Learning Streamlit')
+st.write('This is a simple example of a Streamlit app')
+st.write('We are learning Streamlit')
+st.write('This is a simple example of a Streamlit app')
